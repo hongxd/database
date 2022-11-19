fea833b3e38d35db92128da62a3d77040567b6c2
ddd <d4980@qq.com>
2022-11-19 19:01:40 +0800

    chore: 宿舍楼增加宿舍管理员属性

---
+++ Migrations/20221119110047_宿舍楼增加宿舍管理员属性.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace database.Migrations
+{
+    /// <inheritdoc />
+    public partial class 宿舍楼增加宿舍管理员属性 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "dormmanager",
+                table: "dormbuild",
+                type: "uniqueidentifier",
+                nullable: true,
+                comment: "管理员");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "dormmanager",
+                table: "dormbuild");
+        }
+    }
+}
