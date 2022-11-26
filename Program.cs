b1629cf8f76cefec78da37c5aca816d052c2828a
ddd <d4980@qq.com>
2022-11-26 22:01:22 +0800

    chore: 取消配置设置端口

--- Program.cs
+++ Program.cs
@@ -39,8 +39,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddCors(opt => { opt.AddPolicy("cors", p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()); });
 
 // 配置url端口为8868，没配置默认5000
-var hostUrl = builder.Configuration.GetValue<string>("HostUrl") ?? "";
-builder.WebHost.UseUrls(hostUrl);
+// var hostUrl = builder.Configuration.GetValue<string>("HostUrl") ?? "";
+// builder.WebHost.UseUrls(hostUrl);
 
 var app = builder.Build();
 
